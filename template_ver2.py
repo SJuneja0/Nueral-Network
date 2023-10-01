d4d722c453832854eff8368d5e14dc9bc82d0544
SJuneja0 <101993552+SJuneja0@users.noreply.github.com>
2023-10-01 13:11:55 -0400

    added flowers to directory 'images'

--- template_ver2.py
+++ template_ver2.py
@@ -8,10 +8,12 @@ import numpy as np
 # Change the hyper-parameters to get the model performs well
 config = {
     'batch_size': 64,
-    'image_size': (30,30),
+    'image_size': (30, 30),
     'epochs': 20,
     'optimizer': keras.optimizers.experimental.SGD(1e-2)
 }
+
+
 ###########################MAGIC ENDS  HERE##########################
 
 def read_data():
@@ -23,12 +25,13 @@ def read_data():
         image_size=config['image_size'],
         batch_size=config['batch_size'],
         labels='inferred',
-        label_mode = 'int'
+        label_mode='int'
     )
     val_batches = tf.data.experimental.cardinality(val_ds)
     test_ds = val_ds.take(val_batches // 2)
     val_ds = val_ds.skip(val_batches // 2)
     return train_ds, val_ds, test_ds
+
 
 def data_processing(ds):
     data_augmentation = keras.Sequential(
@@ -47,9 +50,10 @@ def data_processing(ds):
     ds = ds.prefetch(tf.data.AUTOTUNE)
     return ds
 
+
 def build_model(input_shape, num_classes):
     inputs = keras.Input(shape=input_shape)
-    x = layers.Rescaling(1./255)(inputs)
+    x = layers.Rescaling(1. / 255)(inputs)
     ###########################MAGIC HAPPENS HERE##########################
     # Build up a neural network to achieve better performance.
     # Use Keras API like `x = layers.XXX()(x)`
@@ -66,13 +70,12 @@ def build_model(input_shape, num_classes):
     return model
 
 
-
 if __name__ == '__main__':
     # Load and Process the dataset
     train_ds, val_ds, test_ds = read_data()
     train_ds = data_processing(train_ds)
     # Build up the ANN model
-    model = build_model(config['image_size']+(3,), 5)
+    model = build_model(config['image_size'] + (3,), 5)
     # Compile the model with optimizer and loss function
     model.compile(
         optimizer=config['optimizer'],
@@ -91,14 +94,11 @@ if __name__ == '__main__':
     print("\nTest Accuracy: ", test_acc)
     test_images = np.concatenate([x for x, y in test_ds], axis=0)
     test_labels = np.concatenate([y for x, y in test_ds], axis=0)
-    test_prediction = np.argmax(model.predict(test_images),1)
+    test_prediction = np.argmax(model.predict(test_images), 1)
     # 1. Visualize the confusion matrix by matplotlib and sklearn based on test_prediction and test_labels
     # 2. Report the precision and recall for 10 different classes
     # Hint: check the precision and recall functions from sklearn package or you can implement these function by yourselves.
     # 3. Visualize three misclassified images
     # Hint: Use the test_images array to generate the misclassified images using matplotlib
 
-
-
-
-    ###########################MAGIC HAPPENS HERE##########################
+    ###########################MAGIC ENDS HERE##########################
